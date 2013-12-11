c4a3bb14cacb77c905270bf48c584132c64a1f9a
Niklas Närhinen <niklas@narhinen.net>
2013-12-12 01:42:02 +0200

    Move camt060 to core

--- src/corporate/core.clj
+++ src/corporate/core.clj
@@ -62,4 +62,30 @@
                                                                                                         (str (or (:rf-reference-number payment)
                                                                                                             (:reference-number payment))))))))) (:payments payment-group))))))))
 
-
+(defn camt-060-001-02 [data]
+  (let [now (ft/unparse (:date-hour-minute-second ft/formatters) (tm/now))]
+   (xml/element
+    :Document
+    {:xmlns "urn:iso:std:iso:20022:tech:xsd:camt.060.001.02"
+     :xmlns:xsi "http://www.w3.org/2001/XMLSchema-instance"
+     :xsi:schemaLocation "urn:iso:std:iso:20022:tech:xsd:camt.060.001.02 camt.060.001.02.xsd"}
+    (xml/element :AcctRptgReq {}
+                 (xml/element :GrpHdr {}
+                              (xml/element :MsgId {} (str (:message-identification data)))
+                              (xml/element :CreDtTm {} now))
+                 (xml/element :RptgReq {}
+                              (xml/element :Id {} (:identification data))
+                              (xml/element :ReqdMsgNmId {} (:requested-message-name-identification data))
+                              (xml/element :Acct {}
+                                           (xml/element :Id {}
+                                                        (xml/element :IBAN {} (:iban data))))
+                              (xml/element :AcctOwnr {}
+                                           (xml/element :Pty {}
+                                                        (xml/element :Nm {} (:account-owner-name data))))
+                              (xml/element :RptgPrd {}
+                                           (xml/element :FrToDt {}
+                                                        (xml/element :FrDt {} (:from-date data)))
+                                           (xml/element :FrToTm {}
+                                                        (xml/element :FrTm {} (:from-time data)))
+                                           (xml/element :Tp {} (:type data))))))))
+ 
